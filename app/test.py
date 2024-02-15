5ffff958fc3cfe54def8d8f866d652e03e332b04
Oliver Zein <oliverzein@gmail.com>
2024-02-15 16:13:32 +0100

    .

--- app/test.py
+++ app/test.py
@@ -1,42 +1,15 @@
-import struct, js_to_hid
+from zero_hid import Keyboard, Mouse
 
-release_key = b'\x02\x00\x00\x00\x00\x00\x00\x00\x00'
+k = Keyboard()
+k.set_layout(language='DE_ASCII')
+#k.type("Hello world! zzz #+\\")
 
-def int_to_byte(n: int) -> bytes:
-    return struct.pack('<B', n & 0xff)
-  
-def hidBuffersFromString(string):
-    hidBuffer = []
-    
-    for char in string:
-        ascval = ord(char)
-        print('Char: %s = %d', char, ascval)
-        control_keys, hid_keycode = js_to_hid.convert2(ascval)
-        print(hid_keycode)
-        byteArray = [2, 0, 0, hid_keycode, 0, 0, 0, 0, 0]
-        bytes = b''.join([*map(int_to_byte, byteArray)])
-        hidBuffer.append(bytes)
+""" with Mouse() as m:
+    for i in range(5):
+        m.move(5,5) """
         
-    return hidBuffer
-
-def sendHIDBuffer(hidBuffer):
-    with open('/dev/hidg0', 'rb+') as fd:
-        for bytes in hidBuffer:
-            fd.write(bytes)
-            fd.flush()
-        
-def sendChar(char):
-    ascval = ord(char)
-    print('Char: %s = %d', char, ascval)
-    control_keys, hid_keycode = js_to_hid.convert2(ascval)
-    print(hid_keycode)
-
-    bytebuffer = [2, 0, 0, hid_keycode, 0, 0, 0, 0, 0]
-    bytes = b''.join([*map(int_to_byte, bytebuffer)])
-    with open('/dev/hidg0', 'rb+') as fd:
-            fd.write(bytes)
-            fd.flush()
-            fd.write(release_key)
-
-hidBuffer = hidBuffersFromString("Hallo")
-sendHIDBuffer(hidBuffer=hidBuffer)
+with Mouse() as m:
+    m.move(5,0)
+    m.move(0,-5)
+    m.move(-5,0)
+    m.move(0,5)
